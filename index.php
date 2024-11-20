016f5b9163c90857cc26f772ad81721c80de9489
anubizz77 <foxywolf984@gmail.com>
2024-11-20 12:05:20 -0400

    Feat:changes

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,
+initial-scale=1.0">
+<link rel="stylesheet" href="./style.css">
+<title>Home</title>
+</head>
+<body>
+<?php include './conexion.php' ?>
+<?php include './menu.php' ?>
+<img src="ai-generated-8682714_1280.png" alt="imagen de la página de
+inicio" class="main-image">
+</body>
+</html>
